1add64f80a6919a087db73d09d37eb62092ecb0f
zogodo <742782908@qq.com>
2023-01-30 16:16:04 +0800

    up

--- app/src/main/java/me/zogodo/androiddemo/MyUsage.java
+++ app/src/main/java/me/zogodo/androiddemo/MyUsage.java
@@ -1,23 +1,18 @@
 package me.zogodo.androiddemo;
 
-import android.app.usage.EventStats;
 import android.app.usage.UsageEvents;
 import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.util.Log;
 
-import java.sql.Date;
 import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
 
 public class MyUsage
 {
     public static void TestEvent(Context context)
     {
         long endTime = System.currentTimeMillis();
-        long beginTime = endTime - 1000 * 60 * 60 * 3; //最近3小时
+        long beginTime = endTime - 1000 * 60 * 10; //最近10分钟
         UsageStatsManager manager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
         UsageEvents usageEvents = manager.queryEvents(beginTime, endTime);
 
@@ -28,7 +23,10 @@ public class MyUsage
             usageEvents.getNextEvent(eventOut);
             Timestamp ts = new Timestamp(eventOut.getTimeStamp());
             int t = eventOut.getEventType();
-            if (t == UsageEvents.Event.SCREEN_INTERACTIVE || t == UsageEvents.Event.SCREEN_NON_INTERACTIVE)
+            if (t == UsageEvents.Event.SCREEN_INTERACTIVE         //亮屏
+                || t == UsageEvents.Event.SCREEN_NON_INTERACTIVE  //灭屏
+                || t == UsageEvents.Event.KEYGUARD_SHOWN          //锁屏
+                || t == UsageEvents.Event.KEYGUARD_HIDDEN)        //解锁
             {
                 Log.e("zzze1", ts.toString() + " t = " + eventOut.getEventType(), null);
             }
